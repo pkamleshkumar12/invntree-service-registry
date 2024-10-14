1bf62f6fdd9a3a3ec71181e810939e196421eea1
kamlesh <pkamleshkumar12@gmail.com>
2024-10-14 20:34:37 +1300

    Added Eureka registry configurations

---
+++ src/test/java/com/invntree/serviceregistry/InvntreeServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invntree.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvntreeServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
